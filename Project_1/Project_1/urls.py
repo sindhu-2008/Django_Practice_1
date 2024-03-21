21e0df89f9af7e54955e16e928267c712fa3324b
sindhu <sindhumj>
2024-03-21 16:59:19 +0530

    committed urls.py

--- Project_1/Project_1/urls.py
+++ Project_1/Project_1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App_1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("h",views.func,name="function"),
 ]
